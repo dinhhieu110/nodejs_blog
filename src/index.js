56f3729266de9926b7315bc2c575f55c66259634
Tran Dinh Hieu <davidtranit110@gmail.com>
2023-12-12 22:32:49 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,8 +5,17 @@ const handlebars = require("express-handlebars");
 const app = express(); // Call function express return an object and assigned to app variable
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
-app.use(morgan("combined"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+); // middleware xử lí submitform với method POST
+app.use(express.json());
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine(
@@ -18,14 +27,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-// Route
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
